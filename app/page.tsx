6e3a6f71799d00a7e529ec304b8b176c61457cec
SmirkzyyDBD <smirkinator93@gmail.com>
2025-02-11 18:56:17 +0000

    rolled back to older version, updated filenames

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
             <div className="space-y-4">
               <DownloadButton fileName="670_ptb.7z" text="Game content" />
               <DownloadButton
-                fileName="EefernalFogPSv2.3.zip"
+                fileName="EefernalFogPSv2.4.zip"
                 text="Patch files"
               />
             </div>
@@ -40,7 +40,7 @@ export default function Home() {
                 text="Game content"
               />
               <DownloadButton
-                fileName="EefernalFogPSv3.1.zip"
+                fileName="EefernalFogPSv3.2.zip"
                 text="Patch files"
               />
             </div>
