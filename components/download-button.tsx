d6ccdbce163f9d65fffd258c95211cb822deafee
SmirkzyyDBD <smirkinator93@gmail.com>
2025-03-26 15:22:47 +0000

    Added modals for downloads, to make things tidy. added extra game versions.

--- components/download-button.tsx
+++ components/download-button.tsx
@@ -6,9 +6,14 @@ import React, { useState } from "react";
 interface DownloadButtonProps {
   fileName: string;
   text?: string;
+  className?: string;
 }
 
-const DownloadButton: React.FC<DownloadButtonProps> = ({ fileName, text }) => {
+const DownloadButton: React.FC<DownloadButtonProps> = ({
+  fileName,
+  text,
+  className = "",
+}) => {
   const [loading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
 
@@ -37,24 +42,22 @@ const DownloadButton: React.FC<DownloadButtonProps> = ({ fileName, text }) => {
   };
 
   return (
-    <div className="space-y-2">
+    <div className={`space-y-2 ${className}`}>
       <button
         onClick={handleDownload}
         disabled={loading}
-        className={`relative inline-block w-48 p-px font-semibold tracking-tight text-white no-underline cursor-pointer rounded-xl ${
-          loading ? "opacity-50 cursor-not-allowed" : ""
-        }`}
+        className={`relative inline-flex w-full justify-center items-center px-6 py-3 space-x-2 font-semibold tracking-tight text-white rounded-xl ${
+          loading ? "opacity-50 cursor-not-allowed" : "hover:bg-white/10"
+        } transition-colors duration-300 backdrop-blur ring-1 ring-white/10`}
       >
-        <div className="relative z-10 flex items-center justify-center px-6 py-3 space-x-2 rounded-xl hover:bg-white/10 transition-colors duration-300 backdrop-blur ring-1 ring-white/10">
-          <span>
-            {loading ? (
-              <LoaderCircle className="animate-spin" />
-            ) : (
-              text || fileName
-            )}
-          </span>
-          <ChevronRight className="w-4 h-4" />
-        </div>
+        <span>
+          {loading ? (
+            <LoaderCircle className="animate-spin" />
+          ) : (
+            text || fileName
+          )}
+        </span>
+        <ChevronRight className="w-4 h-4" />
       </button>
       {error && <p className="text-red-500 text-sm">{error}</p>}
     </div>
