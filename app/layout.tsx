41999d76416d9f79a238ebb8bfdf0cea1eeaa3f4
jacobrozwadowski <xype.discord@gmail.com>
2025-05-14 16:08:39 +0200

    chore: remove plausible analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,16 @@
 import type { Metadata, Viewport } from "next";
 import { Inter } from 'next/font/google'
 import "./globals.css";
-import PlausibleProvider from "next-plausible";
 
 const inter = Inter({ subsets: ["latin"]})
+
 export const metadata: Metadata = {
   title: "Eefernal Fog CDN",
   description:
     "High-performance content delivery network for Eefernal Fog Private Server.",
   keywords:
     "CDN, Dead by Daylight, gaming, downloads, fast delivery, dbd, private server",
-  authors: [{ name: "Eefernal Fog" }],
+  authors: [{ name: "smirkzyy, 1w6ts" }],
 };
 
 export const viewport: Viewport = {
@@ -24,13 +24,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <PlausibleProvider domain="cdn.eefernal.cc">
         <body
           className={`${inter.className} antialiased min-h-screen`}
         >
           <main className="relative">{children}</main>
         </body>
-      </PlausibleProvider>
     </html>
   );
 }
